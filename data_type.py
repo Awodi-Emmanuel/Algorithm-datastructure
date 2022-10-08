bf3253979f020ffa631c45096a335a9efb8a8b81
awodi-emmanuel <emmanuelawodi25@gmail.com>
2022-10-08 15:49:45 +0100

    Hackarant solution on data type

---
+++ data_type.py
@@ -0,0 +1,17 @@
+i = 2
+d = 8.0 
+s = "Hackerank"
+# Declear second integer, double, and string variable.
+
+# Read and save an integer, double, and string to your variables.
+input_1 = int(input('Enter an integers: '))
+input_2 = float(input('Enter a decimal : '))
+input_3 = str(input('Enter a string'))
+
+# Print the sum of both integer variables on a new line
+print("Integer: ", i + input_1)
+# print the sum of the double variables  on a new line.
+print("Decimal: ", d + input_2)
+# Concatenate and print the String variables on a new line
+print("String: ", s + input_3)
+# The 's' variable above should be printed first
---
+++ main.py
@@ -0,0 +1,4 @@
+def print_doucment_sytax():
+    print(abs.__doc__)
+
+print_doucment_sytax()
